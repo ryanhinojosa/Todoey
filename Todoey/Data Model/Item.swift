f8ec2b44f39578f2644918b0568a3bf1a1ffaf9e
Ryan Hinojosa <ryan.c.hinojosa@gmail.com>
2018-02-27 14:56:12 -0800

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Ryan Hinojosa on 2/27/18.
+//  Copyright © 2018 Ryan Hinojosa. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+    
+}
